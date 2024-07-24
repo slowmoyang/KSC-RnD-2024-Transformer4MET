19cdedf084ef92f3d1228bf9d7fc7cd7e4d4fdf1
Seungjin Yang <slowmoyang@gmail.com>
2024-07-24 16:41:17 +0900

    add FlatGenMETpTRandomSampler
    add `FlatGenMETpTRandomSampler` sampling events so that a distribution
    of generated MET pT is uniform.
    This sampler is introduced to mitigate the effect of low
    statistics of events with high MET

--- src/diffmet/data/datamodule.py
+++ src/diffmet/data/datamodule.py
@@ -5,11 +5,13 @@ from ..utils.lit import get_class
 
 
 class DataModule(LightningDataModule):
+
     def __init__(self,
                  dataset_class_path: str,
                  train_files: list[str],
                  val_files: list[str],
                  test_files: list[str],
+                 train_sampler_class_path: str | None = None,
                  batch_size: int = 256,
                  eval_batch_size: int = 512,
     ) -> None:
@@ -23,6 +25,8 @@ class DataModule(LightningDataModule):
 
         self.dataset_cls = get_class(dataset_class_path)
 
+        self.train_sampler_class_path = train_sampler_class_path
+
     @cached_property
     def train_set(self):
         return self.dataset_cls.from_root(self.train_files)
@@ -34,6 +38,14 @@ class DataModule(LightningDataModule):
     @cached_property
     def test_set(self):
         return self.dataset_cls.from_root(self.test_files)
+
+    @cached_property
+    def train_sampler(self):
+        if self.train_sampler_class_path is not None:
+            sampler_class = get_class(self.train_sampler_class_path)
+            # TODO: num_samples is assuemd to be same with len(self.train_set)
+            sampler = sampler_class(self.train_set)
+            return sampler
 
     def setup(self, stage: str) -> None:
         if stage == 'fit':
@@ -50,18 +62,26 @@ class DataModule(LightningDataModule):
         if stage == 'fit':
             delattr(self, 'train_set')
             delattr(self, 'val_set')
+            delattr(self, 'train_sampler')
         elif stage == 'test':
             delattr(self, 'test_set')
 
 
     def train_dataloader(self):
         dataset = self.train_set
+
+        kwargs = {}
+        if self.train_sampler is None:
+            kwargs['shuffle'] = True
+        else:
+            kwargs['sampler'] = self.train_sampler
+
         return DataLoader(
             dataset=dataset,
             batch_size=self.batch_size,
-            shuffle=True,
             collate_fn=dataset.collate,
             drop_last=True,
+            **kwargs
         )
 
     def _eval_dataloader(self, dataset):
---
+++ src/diffmet/data/samplers/sampler.py
@@ -0,0 +1,44 @@
+import warnings
+import torch
+from torch import Tensor
+from torch.utils.data import WeightedRandomSampler
+
+
+class FlatGenMETpTRandomSampler(WeightedRandomSampler):
+    """
+    This sampler aims at sampling events so that generated MET pT distribution
+    is flat.
+    """
+
+    def __init__(self,
+                 dataset,
+                 boundaries: Tensor | None = None,
+    ):
+        """
+        Args:
+            dataset: dataset, where gen met are taken
+            boundaries: boundaries of a gen met pt histogram
+        """
+        boundaries = boundaries or torch.linspace(0, 400, 41)
+
+        # gen_met is supposed to be (px, py)
+        gen_met_pt = [each['gen_met'].norm(p=2) for each in dataset]
+        gen_met_pt = torch.stack(gen_met_pt)
+        gen_met_pt.clip_(boundaries[0], boundaries[-1])
+
+        hist, _ = torch.histogram(input=gen_met_pt, bins=boundaries)
+        if hist.eq(0).any():
+            warnings.warn(message='found empty bin')
+            hist.clamp_(1, None) # FIXME warning
+
+        pdf = hist / len(hist)
+
+        bins = torch.bucketize(gen_met_pt, boundaries=boundaries, right=False) - 1
+
+        # sampling weights
+        weights = 1 / pdf[bins]
+
+        super().__init__(
+            weights=weights.tolist(),
+            num_samples=len(weights)
+        )
