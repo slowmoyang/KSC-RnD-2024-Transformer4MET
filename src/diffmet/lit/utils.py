4ca458760c24a140da35288ccf18b831da2cf698
Seungjin Yang <slowmoyang@gmail.com>
2024-08-30 15:47:23 +0900

    a few minor updates
    - move lr_find to diffmet package
    - add a function get the next version of log dir
    - add a flag to LitCLI to turn on lr_find
    - load all states including model and optimzers when loading ckpt

--- src/diffmet/lit/utils.py
+++ src/diffmet/lit/utils.py
@@ -1,4 +1,9 @@
+import json
+from pathlib import Path
 from typing import Type
+import torch
+from lightning.pytorch.tuner.tuning import Tuner
+import matplotlib.pyplot as plt
 
 # FIXME importlib?
 def get_class(class_path: str) -> Type:
@@ -8,3 +13,61 @@ def get_class(class_path: str) -> Type:
     class_module, class_name = class_path.rsplit(".", 1)
     module = __import__(class_module, fromlist=[class_name])
     return getattr(module, class_name)
+
+
+
+def run_lr_find(trainer, model, datamodule, ckpt_path: Path | None):
+    if ckpt_path is not None:
+        print(f'🤖 load {ckpt_path}')
+        ckpt = torch.load(ckpt_path)
+        model.load_state_dict(ckpt['state_dict'])
+        del ckpt
+
+    tuner = Tuner(trainer)
+    lr_finder = tuner.lr_find(model, datamodule=datamodule)
+    assert lr_finder is not None
+
+    assert trainer.log_dir is not None
+    log_dir = Path(trainer.log_dir)
+
+    output_dir = log_dir / 'lr-find'
+    output_dir.mkdir()
+
+    with open(output_dir / 'results.json', 'w') as stream:
+        output = {
+            'suggestion': lr_finder.suggestion(),
+            'results': lr_finder.results,
+        }
+        json.dump(output, stream, indent=2)
+
+    fig = lr_finder.plot(suggest=True)
+    assert fig is not None
+
+    output_path = output_dir / 'plot'
+    for suffix in ['.png', '.pdf']:
+        fig.savefig(output_path.with_suffix(suffix))
+    plt.close(fig)
+
+
+def get_next_version(root: Path) -> int:
+    """
+    adapted from https://github.com/Lightning-AI/pytorch-lightning/blob/2.4.0/src/lightning/fabric/loggers/csv_logs.py#L171-L189
+    """
+    if not root.exists():
+        raise FileNotFoundError(root)
+    if not root.is_dir():
+        raise NotADirectoryError(root)
+
+    existing_versions: list[int] = []
+    for each in root.glob('version_*'):
+        if each.is_dir():
+            dir_ver = each.name.split("_")[1]
+            if dir_ver.isdigit():
+                existing_versions.append(int(dir_ver))
+        # else: warn
+
+    return max(existing_versions) + 1 if len(existing_versions) > 0 else 0
+
+def get_next_version_name(root: Path) -> str:
+    version_num = get_next_version(root)
+    return f'version_{version_num:d}'
--- train.py
+++ train.py
@@ -1,65 +1,55 @@
 #!/usr/bin/env python
-import json
 from pathlib import Path
 import torch
 from lightning.pytorch.cli import LightningCLI
 from lightning.pytorch.trainer import Trainer
-from lightning.pytorch.tuner.tuning import Tuner
 import matplotlib as mpl
-import matplotlib.pyplot as plt
 import mplhep as mh
 from diffmet.lit.model import LitModel
 from diffmet.lit.datamodule import DataModule
+from diffmet.lit.utils import run_lr_find
 mpl.use('agg')
 mh.style.use(mh.styles.CMS)
 
 
-def run_lr_find(trainer, model, datamodule):
-    tuner = Tuner(trainer)
-    lr_finder = tuner.lr_find(model, datamodule=datamodule)
-    assert lr_finder is not None
+class LitCLI(LightningCLI):
 
-    assert trainer.log_dir is not None
-    log_dir = Path(trainer.log_dir)
-
-    output_dir = log_dir / 'lr-find'
-    output_dir.mkdir()
-
-    with open(output_dir / 'results.json', 'w') as stream:
-        output = {
-            'suggestion': lr_finder.suggestion(),
-            'results': lr_finder.results,
-        }
-        json.dump(output, stream, indent=2)
-
-    fig = lr_finder.plot(suggest=True)
-    assert fig is not None
-
-    output_path = output_dir / 'plot'
-    for suffix in ['.png', '.pdf']:
-        fig.savefig(output_path.with_suffix(suffix))
-    plt.close(fig)
+    def add_arguments_to_parser(self, parser):
+        parser.add_argument("--ckpt", dest='ckpt_path', type=Path,
+                            help='checkpoint')
+        parser.add_argument("--lr-find", action='store_true',
+                            help='run learning rate finding')
 
 
 def run(trainer: Trainer,
         model: LitModel,
         datamodule: DataModule,
+        config,
 ):
     torch.set_num_threads(1)
 
-    run_lr_find(trainer=trainer, model=model, datamodule=datamodule)
+    ckpt_path = config['ckpt_path']
 
-    trainer.validate(model, datamodule=datamodule)
-    trainer.fit(model, datamodule=datamodule)
-    trainer.test(ckpt_path='best', datamodule=datamodule)
+    if config['lr_find']:
+        print('🚀 LEARNING RATE FINDING')
+        run_lr_find(trainer=trainer, model=model, datamodule=datamodule,
+                    ckpt_path=ckpt_path)
 
+    print('🚀 VALIDATION')
+    trainer.validate(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+
+    print('🚀 FIT')
+    trainer.fit(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+
+    print('🚀 TEST')
+    trainer.test(model=model, datamodule=datamodule, ckpt_path='best')
 
 
 def main():
-    cli = LightningCLI(
+    cli = LitCLI(
         model_class=LitModel,
         datamodule_class=DataModule,
-        seed_everything_default=1234,
+        seed_everything_default=1337,
         run=False, # used to de-activate automatic fitting.
         trainer_defaults={
             'max_epochs': 10,
@@ -78,7 +68,8 @@ def main():
     run(
         trainer=cli.trainer,
         model=cli.model,
-        datamodule=cli.datamodule
+        datamodule=cli.datamodule,
+        config=cli.config,
     )
 
 
